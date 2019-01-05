6db4d0fec9be17640682892615c70eb3a80afb61
Jáchym Tomášek <jachym.tomasek@gmail.com>
2019-01-05 11:48:58 +0100

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+puts "Initializing setup scripts"
+puts "You\'ve done it! Congratulations"
+puts "Now it\'s our problem if something isn\'t working"
+puts "Make yourself some coffee, we\'ll be done soon enough (I hope)"
